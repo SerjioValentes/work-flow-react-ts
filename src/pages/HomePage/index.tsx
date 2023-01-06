a6d0ab06a2e732103f7178d12696cf2c69fb3a49
SerjioValentes <vasilkamalov@gmail.com>
2023-01-06 17:26:15 +0700

    delete key

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import { Button, Divider, TextField } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import React, { useState } from 'react';
 // import { getAuth, signOut } from "firebase/auth";
 // import fire from "../../firebase";
 
 const HomePage = () => {
+
+    
     // const auth = getAuth(fire);
+
+    // const chanelLink = {
+    //     users: ['user01@test.com', 'user02@test.com'],
+    //     dateOfCreation: new Date,
+    // }
+
+    const createLink = () => {
+        console.log('oj');
+        
+    }
 
     return (
         <div>
-            Home Page
+            <div>
+
+              <div>
+            <TextField 
+            label='enterUsers'
+            />
+            <Button>Добавить пользователя</Button>
+            </div>  
+            <Divider sx={{my: 4}} />
+            <Button
+            onChange={createLink}
+            >Создать ссылку</Button>
+            </div>
+            
+
+            
         </div>
     );
 };
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,11 @@
-import GiftBoardPage from "../pages/GiftBoardPage";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
-import TasksBoardPage from "../pages/TasksBoardPage";
 
 export const routes: any = [
     {
         path: '/',
         name: 'Home page',
         component: <HomePage/>,
-    },
-    {
-        path: '/tasksBoard',
-        name: 'Task board',
-        component: <TasksBoardPage/>,
-    },
-    {
-        path: '/giftBoard',
-        name: 'Gift board',
-        component: <GiftBoardPage />,
     },
 ]
 
